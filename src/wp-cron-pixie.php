d7c491bb9dc8f4cfebc5252221336fed0370a097
Ian M. Jones <ian@ianmjones.com>
2017-02-09 20:31:52 +0000

    Made metwork activate only.
    Plugin does not actually support multisite networks as yet.

--- src/wp-cron-pixie.php
+++ src/wp-cron-pixie.php
@@ -15,7 +15,7 @@
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       wp-cron-pixie
  * Domain Path:       /languages
- * Network:           False
+ * Network:           True
  */
 
 // If this file is called directly, abort.
