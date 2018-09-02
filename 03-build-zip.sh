57a0dfa0b80075a19db31803d7c3776e1ddf2f61
Ian M. Jones <ian@ianmjones.com>
2018-09-02 11:24:08 +0100

    Added build script for the zip.

---
+++ 03-build-zip.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+
+function usage() {
+	echo "Usage: $0 [ slug1 slug2 ... ]"
+	exit 1
+}
+
+SCRIPT_ARGS="$@"
+PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PLUGIN_SRC_PATH="$PROJECT_ROOT/src"
+PLUGIN_BUILDS_PATH="$PROJECT_ROOT/builds"
+PLUGIN_BUILD_CONFIG_PATH="$PROJECT_ROOT/build-cfg"
+
+function echog() {
+    echo "$(tput setaf 2)$1$(tput sgr 0)"
+}
+function echor() {
+    echo "$(tput setaf 1)$1$(tput sgr 0)"
+}
+
+function build_plugin() {
+    PLUGIN_DIR="$PLUGIN_BUILD_CONFIG_PATH/$PLUGIN"
+
+    if [[ -d "$PLUGIN_DIR" && ! -L "$PLUGIN_DIR" ]]; then
+        if [ "utils" != "$PLUGIN" ] && [ "common" != "$PLUGIN" ];
+        then
+            VERSION=$(php -f "$PLUGIN_BUILD_CONFIG_PATH/utils/get_plugin_version.php" "$PROJECT_ROOT" $PLUGIN)
+            ZIP_NAME=$(php -f "$PLUGIN_BUILD_CONFIG_PATH/utils/get_plugin_zip_name.php" "$PROJECT_ROOT" $PLUGIN)
+            BUILD_ZIP="$PLUGIN_BUILDS_PATH/$ZIP_NAME-$VERSION.zip";
+
+            if [ -f "$BUILD_ZIP" ]
+            then
+                rm "$BUILD_ZIP"
+            fi
+
+            echog "Building $PLUGIN v$VERSION..."
+            cd "$PLUGIN_BUILD_CONFIG_PATH/$PLUGIN/"
+            "$PLUGIN_BUILDS_PATH/plugin-build" "$VERSION"
+            echog "Plugin built: $BUILD_ZIP"
+            echo "--------------------------"
+        fi
+    else
+        usage
+    fi
+}
+
+cd "$PROJECT_ROOT";
+
+if [ -z "${SCRIPT_ARGS}" ]
+then
+    echog "Clearing previously built plugins..."
+    rm -rf "$PLUGIN_BUILDS_PATH"
+fi
+
+if [ ! -d "$PLUGIN_BUILDS_PATH" ]
+then
+    mkdir -p "$PLUGIN_BUILDS_PATH"
+fi
+
+if [ ! -f "$PLUGIN_BUILDS_PATH/plugin-build" -o ! -x "$PLUGIN_BUILDS_PATH/plugin-build" ]
+then
+    echog "Downloading plugin-build script..."
+    curl -sSL https://raw.githubusercontent.com/deliciousbrains/wp-plugin-build/582fdeb3f6d19ae0b1f2bd0da9b48f45c131ac34/plugin-build -o "$PLUGIN_BUILDS_PATH/plugin-build"
+    chmod +x "$PLUGIN_BUILDS_PATH/plugin-build"
+fi
+
+echog "Building plugins..."
+if [ -z "${SCRIPT_ARGS}" ]
+then
+    for PLUGIN_DIR in "$PLUGIN_BUILD_CONFIG_PATH"/*/
+    do
+        PLUGIN=$(basename "$PLUGIN_DIR");
+
+        build_plugin
+    done
+else
+    for PLUGIN in ${SCRIPT_ARGS}
+    do
+        build_plugin
+    done
+fi
